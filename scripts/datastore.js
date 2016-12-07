94e907a6b3aaeefa012cddccc98eab5a477fe6c9
Mark Miller <mark.miller2792@gmail.com>
2016-12-06 21:48:52 -0500

    8.7 Added datastore script and app namespace

---
+++ scripts/datastore.js
@@ -0,0 +1,14 @@
+(function(window){
+  'use strict';
+
+  var App = window.App || {};
+
+  function DataStore() {
+    console.log('running the DataStore function');
+  }
+
+  App.DataStore = DataStore;
+  window.App = App;
+
+
+})(window);
