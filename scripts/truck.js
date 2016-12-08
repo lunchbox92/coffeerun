ec377408b423a57f90418033f90970747f9dd0a6
Mark Miller <mark.miller2792@gmail.com>
2016-12-07 21:51:36 -0500

    8.29 Added prototype functions to truck module

--- scripts/truck.js
+++ scripts/truck.js
@@ -2,9 +2,31 @@
   'use strict';
   var App = window.App||{};
 
-  function Truck() {
+  function Truck(truckId, db) {
+    this.truckId = truckId;
+    this.db = db;
+  }
+
+  Truck.prototype.createOrder = function (order) {
+    console.log('Adding order for ' + order.emailAddress);
+    this.db.add(order.emailAddress, order);
+  }
+
+  Truck.prototype.deliverOrder = function (customerId) {
+    console.log('Delivering order for ' + customerId);
+    this.db.remove(customerId);
+  }
+
+  Truck.prototype.printORders = function() {
+    var customerIdArray = Object.keys(this.db.getAll());
+
+    console.log('Truck #' + this.truckId + ' has pending orders:');
+    customerIdArray.forEach(function(){
+      console.log(this.db.get(id));
+    }.bind(this))
   }
 
   App.Truck = Truck;
   window.App = App;
+
 })(window);
