b295a4ffa38239bca8bad51eadfa029857d384b4
Mark Miller <mark.miller2792@gmail.com>
2016-12-11 21:33:20 -0500

    11.5 Added function to assemble Dom elements

--- scripts/checklist.js
+++ scripts/checklist.js
@@ -13,6 +13,30 @@
         if (this.$element.length === 0) {
             throw new Error('Could not find element with selector: ' + selector);
         }
+
+        function Row(coffeeOrder) {
+            var $div = $('<div></div>', {
+                'data-coffee-order': 'checkbox',
+                'class': 'checkbox'
+            });
+
+            var $label = $('<label></label>');
+
+            var $checkbox = $('<input></input>', {
+                type: 'checkbox',
+                value: coffeeOrder.emailAddress
+            });
+
+            var description = coffeeOrder.size + ' ';
+            if (coffeeOrder.flavor) {
+                description += coffeeOrder.flavor + ' ';
+            }
+
+            description += coffeeOrder.coffee + ' ';
+            description += ' (' + coffeeOrder.emailAddress + ')';
+            description += ' (' + coffeeOrder.strength + 'x)';
+
+        }
     }
 
     App.CheckList = CheckList;
