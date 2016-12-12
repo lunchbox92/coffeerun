7786ffd6e31ff049ce24fb040293d8f9796de830
Mark Miller <mark.miller2792@gmail.com>
2016-12-11 19:35:36 -0500

    Bug FIx.

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -18,9 +18,10 @@
       this.$formElement.on('submit', function (event) {
         event.preventDefault();
 
-        var data = $(this).serializeArray().forEach(function (item) {
+        var data = {};
+        $(this).serializeArray().forEach(function (item) {
           data[item.name] = item.value;
-          console.og(item.name + ' is ' + item.value);
+          console.log(item.name + ' is ' + item.value);
         });
         console.log(data);
         fn(data);
