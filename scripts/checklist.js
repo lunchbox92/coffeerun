749583ee1cdaf85a4354174cc5e81040ecfade5b
Mark Miller <mark.miller2792@gmail.com>
2016-12-18 20:22:57 -0500

    11.9 End of Chapter 9

--- scripts/checklist.js
+++ scripts/checklist.js
@@ -15,6 +15,14 @@
         }
     }
 
+    CheckList.prototype.addClickHandler = function(fn) {
+        this.$element.on('click', 'input', function(event) {
+            var email = event.target.value;
+            this.removeRow(email);
+            fn(email);
+        }.bind(this));
+    }
+
     CheckList.prototype.addRow = function(coffeeOrder) {
         this.removeRow(coffeeOrder.emailAddress);
 
